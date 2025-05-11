8156f94581f31e63e65662d9ec00fab2aebe5a7c
Hitesh Rathore <hiteshrathore.dev123@gmail.com>
2025-05-11 14:08:00 +0530

    D'

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login";
 console.log(login);
 
+
+const Payment = "Payment added";
+
+console.log(Payment);
+
 const footer = "Footer added";
 console.log(footer);
